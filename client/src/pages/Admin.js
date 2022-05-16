c54fe806c1a734325a7c375a63085e6adefc6388
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-05-16 12:15:09 +0300

    client commit

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Admin = () => {
+    return ( 
+        <div>
+            Admin panel
+        </div>
+     );
+}
+ 
+export default Admin;
---
+++ client/src/pages/Basket.js
@@ -0,0 +1,9 @@
+const Basket = () => {
+    return ( 
+        <div>
+            basket
+        </div>
+     );
+}
+ 
+export default Basket;
